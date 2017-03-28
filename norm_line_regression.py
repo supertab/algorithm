86934402f0480137d9454653d8ad8be899a96546
supertab <supertab@126.com>
2017-03-28 15:50:44 +0800

    liner regression v1

--- norm_line_regression.py
+++ norm_line_regression.py
@@ -2,6 +2,8 @@
 import numpy as np
 import pickle, time
 import matplotlib.pyplot as plt
+from pylab import * 
+zhfont = mpl.font_manager.FontProperties(fname='/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc')
 
 # normal equation: theta = (X^T*X)^(-1)*X^T*y
 # 矩阵相乘使用np.dot 求逆: np.linalg.inv
@@ -16,100 +18,81 @@ def gra_descent( theta, X, y, rate=0.1, iter_n=0):
     rate: 学习速率
     iter_n: 迭代次数，当使用迭代次数作为收敛条件时设置
     '''
-
     while iter_n<20:
         iter_n+=1
         print('iter:',iter_n, theta)
         m = len(X) # 样本的个数
-        x_n = len(X[1]) # 样本的特征的个数
+        x_n = len(X[0]) # 样本的特征的个数
         err = np.dot(X,theta)-y # 估值与真实值之间的偏差
         # 计算J的值
         J = 1/(2*m) * (err**2).sum()
         print('J=',J)
-        # 更新theta
-        # 将err扩充成mxn矩阵，直接与X相乘
-
-        # err_expand = np.tile(err,(x_n,1)).T
-        # multi_arr = err_expand*X
-        # tmp = []
-        # for i in range(multi_arr.shape[1]):
-            # tmp.append(multi_arr[0:,i].sum())
-        # tmp = np.array(tmp)
-
         tmp = np.dot(err, X)
         remain = rate*tmp/m
-
         if abs( remain.sum()) > 1e-6:
             theta = theta - remain
         else:
             break
     return theta
 
+def norm_A(A, addcol = True):
+    A_mean = A.mean(axis=0)
+    A_std = np.std(A, axis=0)
+    A_scal = A.max(axis=0) - A.min(axis=0)
+    A = (A-A_mean)/ A_std
+    if addcol:
+        # add first col
+        lenA = len(A[:,0])
+        first = np.ones(lenA).reshape((lenA, 1))
+        A = np.c_[first, A]
+    return A, A_mean, A_std
+
+def format_info(ht):
+    # 构建X
+    size=np.array( [ht[2]], dtype=np.float64).T
+    Attr2 = np.c_[size, size**0.5]
+    X = norm_A(Attr2)
+    price = np.array(ht[5].T, dtype=np.float64)
+    price = norm_A(price, addcol=False)
+    return X, price
+
 def hypothesis(x, theta, x_mean, x_std, y_mean, y_std):
-# def hypothesis( x, theta, A, Y):
-    print('--------- hypothesis ----------')
-    # x = x/ x_std 
-    # x = (x-A.mean(axis=0))/ (A[:,1:].max(axis=0)-A[:,1:].min(axis=0)) 
+    x = np.array([x, x**0.5])
     x = (x-x_mean)/ x_std
-    x = [1, x]
+    x = np.append(np.ones(1), x)
     y = np.dot(theta.T, x)
-    # return y*y_std
-    # return y*(Y.max(axis=0)-Y.min(axis=0)) + Y.mean(axis=0)
     return y*y_std+y_mean
 
 # 去重
-with open('hs_info_1000.pkl','rb') as f:
+with open('hs_info_5000.dict.pkl','rb') as f:
     tmp= pickle.load(f)
 
-beg_t = time.time()
+region_list = [i for i in tmp.keys()]
 
-# 分离出rooms, feet, floor ,price
-ht = houseInfo.T
-
-# 构建X
-fristCol = [1]*len(ht[0])
-Attr=np.array( [fristCol, ht[1], ht[2], ht[4] ], dtype=np.float64).T
-
-Attr2 =np.array( [fristCol, ht[2]], dtype=np.float64).T
-# mean norm Attr2
-attr2_mean = Attr2[:,1:].mean(axis=0)
-attr2_std = np.std(Attr2[:,1:], axis=0)
-attr2_scale = Attr2[:,1:].max(axis=0) - Attr2[:,1:].min(axis=0)
-# Attr2[:,1:] = (Attr2[:,1:])/ attr2_std
-Attr2[:,1:] = (Attr2[:,1:] - attr2_mean)/ attr2_std # mean norm with standard deviation
-# Attr2[:,1:] = (Attr2[:,1:] - attr2_mean)/ attr2_scale # mean norm with scale
-price = np.array(ht[5].T, dtype=np.float64)
-# norm price
-price_mean = price.mean(axis=0)
-price_std = np.std(price, axis=0)
-price_scale = price.max(axis=0) - price.min(axis=0)
-# price = (price )/ price_std
-price = (price - price_mean)/ price_std # mean norm with standard deviation
-# price = (price - price_mean)/ price_scale # mean norm with scale
-
-# test 3 attribute
-test_init = np.array([80,20,10,1])
-theta = normal_equ(Attr, price)
-
-# test 1 attribute
-# theta2 = normal_equ(Attr2, price)
-data_t = np.array([0,0])
-theta_gra  = gra_descent(data_t, Attr2, price, 1)
-end_t = time.time()
-print('cost time: %s'%(end_t-beg_t))
-
-# hypothesis
-feet_in = 130
-# price_out = hypothesis(feet_in, theta_gra, Attr2, 
-price_out = hypothesis(feet_in, theta_gra, attr2_mean, attr2_std, price_mean, price_std)
-# price_out = hypothesis(feet_in, theta_gra, attr2_mean, attr2_scale, price_mean, price_scale)
-print('squera: %.2f, price: %.2f'%(feet_in, price_out))
-
-# plot
-plt.figure(2)
-fet = list(range(-2,10)) 
-pri = [theta_gra[0]+theta_gra[1]*i for i in fet]
-plt.plot((Attr2.T)[1], price, 'rx')
-plt.plot(fet, pri)
+i = 1
+fig = plt.figure(figsize=(6, 6))
+# fig = plt.figure()
+for region in region_list[:6]:
+    houseInfo = np.array(tmp[region])
+    ht = houseInfo.T
+    X, price = format_info(ht)
+    # unwrap
+    testA, testA_mean, testA_std = X
+    price, price_mean, price_std = price
+    theta_init= np.zeros(testA.shape[1])
+    theta = gra_descent(theta_init, testA, price, 0.5)
+    # hypothesis
+    feet_in = list(range(1, 400)) 
+    price_out = [hypothesis(feet, theta, testA_mean, testA_std, price_mean, price_std) for feet in feet_in]
+    # plot
+    pos = '32'+str(i)
+    fig.add_subplot(pos)
+    plt.xlim(0,350), plt.ylim(0,3000)
+    plt.plot(ht[2].T, ht[5].T, 'rx')
+    plt.plot(feet_in, price_out)
+    plt.xlabel('面积(m^2)',fontproperties=zhfont)
+    plt.ylabel('房价(万元）',fontproperties=zhfont)
+    plt.title(region, fontproperties=zhfont)
+    i +=1
+fig.subplots_adjust( wspace=0.5,hspace=1)
 plt.show()
-
---
+++ readme.md
@@ -0,0 +1,10 @@
+# 线性回归（line regression）算法原理
+- [简书_线性回归](http://www.jianshu.com/p/9f6033c36465)
+
+# 算法应用-北京市二手房房价估算
+- 数据来源：[http://bj.5i5j.com/exchange/](http://bj.5i5j.com/exchange/)
+- python爬虫脚本:[house2_info](https://github.com/supertab/spider_py/tree/house2_info)
+- 根据房子的面积来估算房价，选用的拟合函数为
+
+- 海淀区、东城区、西城区、通州区、大兴区、房山区 六个地区的房价拟合曲线
+![](https://im1.shutterfly.com/ng/services/mediarender/THISLIFE/021009631031/media/122861831426/medium/1490686684679/enhance)
